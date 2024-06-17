d64abb564a455e7be5090621ab5d75853167871a
emirsassan <endwse@gmail.com>
2024-06-17 08:59:08 +0300

    changed database schemas

--- src/db/schemas.ts
+++ src/db/schemas.ts
@@ -1,6 +1,17 @@
-import { sql } from "drizzle-orm";
-import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core"
+import { sqliteTable, text, integer, primaryKey } from "drizzle-orm/sqlite-core";
+import { createMany } from "drizzle-orm";
 
-const users = sqliteTable("users", {
-  id: integer("id").primaryKey()
-})
+export const users = sqliteTable("users", {
+  id: integer("id").primaryKey(),
+  email: text("email").notNull().unique(),
+  password: text("password").notNull(),
+});
+
+export const logins = sqliteTable("logins", {
+  id: integer("id").primaryKey(),
+  url: text("url").notNull(),
+  username: text("username").notNull(),
+  password: text("password").notNull(),
+});
+
+export const userLogins = createMany(logins);
