99a80943d2c4ab7af6b43e296a32a1e5cb68c3ce
emirsassan <endwse@gmail.com>
2024-06-20 13:18:11 +0300

    test

--- tests/crypto-test.ts
+++ tests/crypto-test.ts
@@ -2,7 +2,7 @@ import { Encryption } from "../src/lib/encryption";
 
 const encrypt = new Encryption(`${process.env.AES_KEY}`);
 
-const encryptedText = encrypt.encrypt(Buffer.from("hello world"))
+const encryptedText = encrypt.encrypt(Buffer.from("hello world hi"))
 
 const decryptedText = encrypt.decrypt(encryptedText)
 
