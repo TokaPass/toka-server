784ef6d307bd73113a302160539d74870a2ce559
emirsassan <endwse@gmail.com>
2024-06-22 20:30:09 +0300

    now using postgresql as primary database

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 
-const prisma = new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL}`
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
+
 export default prisma;
